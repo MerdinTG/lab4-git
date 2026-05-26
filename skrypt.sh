5e8677bab14ac39ef5f9d81b041817c544b4fdfe
Filip <dsw60306@student.ideis.pl>
2026-05-26 12:52:14 +0200

    Dodano opcje --logs tworzaca 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -4,6 +4,20 @@ case "$1" in
     --date)
         date
         ;;
+
+    --logs)
+        for i in $(seq 1 100)
+        do
+            filename="log${i}.txt"
+
+            echo "Nazwa pliku: $filename" > "$filename"
+            echo "Nazwa skryptu: $0" >> "$filename"
+            echo "Data: $(date)" >> "$filename"
+        done
+
+        echo "Utworzono 100 plikow log."
+        ;;
+
     *)
         echo "Nieznana opcja. Użyj --help"
         ;;
